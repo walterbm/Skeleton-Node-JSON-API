99c1bd76533e9da8ff83611d73d2d85d10420ba7
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-02-27 17:11:51 -0500

    add swagger to document api

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const Hapi = require('hapi');
 const models = require('./src/models');
 const routes = require('./src/routes');
 
+// *** documentation dependencies *** //
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+
 // *** create server *** //
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8000
+    port: 4000
 });
 
 // *** routes *** //
 server.route(routes);
 
 
-server.register({
+server.register([
+  {
     register: require('good'),
     options: {
       reporters: [{
@@ -24,7 +30,19 @@ server.register({
         events: { log: '*', response: '*' }
       }]
     }
-}, function (err) {
+  },
+  Inert,
+  Vision,
+  {
+    register: HapiSwagger,
+    options:  {
+      info: {
+          'title': 'SlashQuo API Documentation',
+          'version': '1.0',
+      }
+    }
+  }
+], function (err) {
 
     if (err) {
         console.error(err);
