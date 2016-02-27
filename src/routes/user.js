8f36f2e8f41c785916a454dcfc06de4ecab4735f
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-02-27 17:22:12 -0500

    add docs to user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,36 +4,66 @@ module.exports = [
   {
   	method: 'GET',
   	path: '/users/{user_id}',
-		handler: function (request, reply) {
-      return reply('user#show');
+    config: {
+      handler: function (request, reply) {
+        return reply('user#show');
+      },
+      description: 'Get one Users',
+      notes: 'Returns the information for the user with id in the route',
+      tags: ['api'],
+      validate: {}
     }
   },
   {
   	method: 'GET',
   	path: '/users',
-		handler: function (request, reply) {
-      return reply('user#index');
+    config: {
+      handler: function (request, reply) {
+        return reply('user#index');
+      },
+      description: 'Get all Users',
+      notes: 'Returns an index view of all users',
+      tags: ['api'],
+      validate: {}
     }
   },
   {
   	method: 'POST',
   	path: '/users',
-    handler: function (request, reply) {
-      return reply('user#new');
+    config: {
+      handler: function (request, reply) {
+        return reply('user#new');
+      },
+      description: 'Create a new User',
+      notes: 'Registers a new user',
+      tags: ['api'],
+      validate: {}
     }
   },
   {
   	method: 'PUT',
   	path: '/users/{user_id}',
-    handler: function (request, reply) {
-      return reply('user#update');
+    config: {
+      handler: function (request, reply) {
+        return reply('user#update');
+      },
+      description: 'Updates a User',
+      notes: 'Update user information',
+      tags: ['api'],
+      validate: {}
     }
   },
   {
   	method: 'DELETE',
   	path: '/users/{user_id}',
-    handler: function (request, reply) {
-      return reply('user#destroy');
+    config: {
+      handler: function (request, reply) {
+        return reply('user#destroy');
+      },
+      description: 'Delete a User',
+      notes: 'Remove a user from the database',
+      tags: ['api'],
+      validate: {}
     }
   }
 ];
