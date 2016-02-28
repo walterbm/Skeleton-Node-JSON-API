a3f7db1be0d63078ed4fbfaec4b3937bbe40476e
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-02-28 01:30:38 -0500

    add contract REST routes

---
+++ src/routes/contract.js
@@ -0,0 +1,61 @@
+'use strict';
+const Schema = require('../validate/contract');
+const Controller = require('../controllers/contract');
+
+module.exports = [
+  {
+  	method: 'GET',
+  	path: '/contracts/{contract_id}',
+    config: {
+      handler: Controller.show,
+      description: 'Get one Contract',
+      notes: 'Returns the information for the contract matching the id in the route',
+      tags: ['api'],
+      validate: Schema.show
+    }
+  },
+  {
+  	method: 'GET',
+  	path: '/contracts',
+    config: {
+      handler: Controller.index,
+      description: 'Get all Contracts',
+      notes: 'Returns an index view of all contracts',
+      tags: ['api'],
+      validate: Schema.index
+    }
+  },
+  {
+  	method: 'POST',
+  	path: '/contracts',
+    config: {
+      handler: Controller.create,
+      description: 'Create a new Contract',
+      notes: 'Registers a new contract',
+      tags: ['api'],
+      validate: Schema.create
+    }
+  },
+  {
+  	method: 'PUT',
+  	path: '/contracts/{contract_id}',
+    config: {
+      handler:Controller.update,
+      description: 'Updates a Contract',
+      notes: 'Update a contract',
+      tags: ['api'],
+      validate: Schema.update
+    }
+  },
+  {
+  	method: 'DELETE',
+  	path: '/contracts/{contract_id}',
+    config: {
+      handler: Controller.destroy,
+      description: 'Delete a Contract',
+      notes: 'Remove a contract from the database',
+      tags: ['api'],
+      validate: Schema.delete
+    }
+  }
+];
