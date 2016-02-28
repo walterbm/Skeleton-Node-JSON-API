420f1a0ae96a8c667acc1a3e8847af979482ce82
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-02-28 03:27:50 -0500

    finish REST routes for Asset

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const user = require('./user');
 const contract = require('./contract');
 const bid = require('./bid');
+const asset = require('./asset');
 
-module.exports = [].concat(user, contract, bid);
+module.exports = [].concat(user, contract, bid, asset);
