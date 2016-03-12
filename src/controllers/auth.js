2b99400a9138e2d9f8a37ceadf7354bd2234afb3
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-03-12 14:10:40 -0500

    add password decryption and authentication

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,17 +1,22 @@
 'use strict';
 const models = require('../models');
 const JWT = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   login: function(request, reply) {
     models.User.findOne({
       where: {
-        email: request.payload.email
+        email: request.payload.email,
       }
     })
     .then(function(user) {
-      let token = JWT.sign(user.id, process.env.SECRET_KEY);
-      return reply({"Authorization": token}).code(200);
+      if (bcrypt.compareSync(request.payload.password, user.encrypted_password)) {
+        let token = JWT.sign(user.id, process.env.SECRET_KEY);
+        return reply({"Authorization": token}).code(200);
+      } else {
+        throw true;
+      }
     })
     .catch(function(e){
       return reply({"Error": "Incorrect credentials"}).code(401);
